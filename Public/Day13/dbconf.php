ca2cef91f982b2059659fd9bb6d94979617d7464
Isuru Shehan <139618233+IsuruShehan99@users.noreply.github.com>
2024-12-01 09:43:53 +0000

    connect with database

--- Public/Day13/dbconf.php
+++ Public/Day13/dbconf.php
@@ -1,17 +1,17 @@
 <?php
-//constant variable
+
 define('SERVERNAME', '127.0.0.1:3306');
 define('USERNAME', 'root');
 define('PASSWORD', 'mariadb');
-define('DBNAME', 'students');
+define('DBNAME', 'bookdb');
 try {
-	//connect with database
+	
 	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
 	if (!$connect) {
 		die("connection failed".mysqli_connect_error());
 	} 
 	else {
-		//echo "Connected successfully";
+		echo "Connected successfully";
 	}
 } 
 catch (Exception $e) {
